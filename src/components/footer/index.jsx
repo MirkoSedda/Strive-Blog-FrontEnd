1852c7c32d34c4b16411a1f0ac2563468954584e
yourusername <kunay69@hotmail.it>
2022-03-02 19:01:23 +0100

    footer navbar and bloglike converted to hooks

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer style={{ paddingTop: 50, paddingBottom: 50 }}>
-        <Container>{`${new Date().getFullYear()} - © Strive School | Developed for homework projects.`}</Container>
-      </footer>
-    );
-  }
+const Footer = () => {
+  return (
+    <footer style={{ paddingTop: 50, paddingBottom: 50 }}>
+      <Container>{`${new Date().getFullYear()} - © Strive School | Developed for homework projects.`}</Container>
+    </footer>
+  );
 }
+
+export default Footer;
