75f4396b50d8df27d0b33d48bb7882d390694062
yourusername <kunay69@hotmail.it>
2022-03-02 18:56:51 +0100

    blog components converted to hooks

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
-export default class BlogList extends Component {
-  render() {
-    return (
-      <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
-        ))}
-      </Row>
-    );
-  }
+const BlogList = () => {
+  return (
+    <Row>
+      {posts.map((post) => (
+        <Col md={4} style={{ marginBottom: 50 }}>
+          <BlogItem key={post.title} {...post} />
+        </Col>
+      ))}
+    </Row>
+  );
 }
+
+export default BlogList;
